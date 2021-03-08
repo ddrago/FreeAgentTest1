ae33b0f1542378f3c0960902c13fe81f12210748
ddrago <dragodiego286@gmail.com>
2021-03-08 22:42:57 +0100

    Final changes and corrections. Made the code more long-term mantainable

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
 # FreeAgent Coding Challenge
 
-Thank you for your interest in the FreeAgent Coding Challenge.  This template is a barebones guide to get you started.  Please add any gems, folders, files, etc. you see fit in order to produce a solution you're proud of.
-
-## Coding Challenge Instructions
-
-Please see the INSTRUCTIONS.md file for more information.
+## Requirements
+require 'json'
+require 'date'
 
 ## Your Solution Setup and Run Instructions
 
-Please include instructions on how to setup and run your solution here.
+Minimimum setup is required. To have access to the methods, just have "require 'currency_exchange' " on top of the code where these methods are needed (just like it is done in currency_exchange_test.rb). 
+
+Instructions: 
+    to use the method 'rate', the developer must provide the parametres for a Date object (the date of the required exchange rate), a from_currency string and a to_currency string, both formatted as a three letter string.  
+
 
 ## Your Design Decisions
 
-We'd love to hear your thoughts around any design decisions you made while coding your solution.
+# get_data() method
+
+I went for a get data method. This allows the developer more flexibility, in case they want to change the source of the data. 
+By promoting the separation of concerns, this design choice saves maintenence time and effort. 
+
+Exception handlign is in place for errors relating to the data file and those relating to the parsing of the JSON file. 
+If a specific exception isn't caught, rescue a general exception and print the traceback and the message. More details on this same 
+design strategy in the "# rate() method" section. 
+
+The constant variable "EUR" allows the user to more flexibily change the currency of reference depending on the database
+
+
+# rate() method
+
+The from_currency and to_currency strings are manually capitalized to prevent potential input errors.
+
+A custom Exception class handles input errors like wrong date, from_currency or to_currency given. 
+This accompanies all the other exception handling, which uses standard classes. 
+
+To cover all possible cases, I tended to raise specific Exceptions when possible, and cast a general exception handling
+(see lines like "rescue => e") to catch whatever exception I might have not thought of. 
+This allows for the program to not crash as often during the development process, allowing us to catch and interpret 
+general errors like these. This is further aided by the printing, in these exceptional cases, of the backtrace.
+
+In every case an exception occurs, -1.0 (a float so as to prevent possible type errors with an exterior program using these 
+methods) is output instead of the exchange rate.
+
+Originally I had an exception handling for retrieving the strong from the Date object, but I couldn't thing of a possible 
+exception that wouldn't occur before rather than in the rate() method.
+
+The constant variable "EUR" allows the user to more flexibily change the currency of reference depending on the database
+
+## developer notes
+
+This is the first time I've coded in Ruby, I'm not familiar with the language and some of the solutions I propose might 
+be naively implemented. Given the chance of studying the language a bit more, I'd be able to provide a more concise/safe 
+solution. 
--- lib/currency_exchange.rb
+++ lib/currency_exchange.rb
@@ -1,25 +1,88 @@
+require 'json'
+require 'date'
+
 module CurrencyExchange
 
-# Return the data given as a dictionary
-# Raises an exception if data not found --> In this case where the data comes from a file, exception raised if file not found
-def self.get_data()
-	#open file and return the data as a dictionary
+	CURRENCY_OF_REFERENCE = "EUR"
+	DATA_FILEPATH = "data/eurofxref-hist-90d.json"
 
+	class InputError < StandardError
+		def initialize(msg="Faulty input", exception_type="custom")
+			@exception_type = exception_type
+			super(msg)
+		end
+	end
 
-	return something
-end
+	# Return the data given as a dictionary
+	# Raises an exception if data not found --> In this case where the data comes from a file, exception raised if file not found 
+	# or if it is not parsed correctly
+	def self.get_data()
+		error_header_string = "FILE RETRIEVAL ERROR: "
+		exchange_rates = {}
+	
+		File.open(DATA_FILEPATH, "r") do |file|
+			json_file = file.read()
+			exchange_rates = JSON.parse(json_file)
+		end
+	
+	rescue Errno::ENOENT, Errno::EACCES, JSON::ParserError => e  
+		puts error_header_string + e.to_s
+	rescue => e
+		puts error_header_string + e.to_s
+		puts e.backtrace.inspect
+	else
+		return exchange_rates
+	end
+	
 
-# Return the exchange rate between from_currency and to_currency on date as a float.
-# Raises an exception if unable to calculate requested rate.
-# Raises an exception if there is no rate for the date provided.
-def self.rate(date, from_currency, to_currency)
-	# 1$ = x*1€
-	# 1£ = y*1€, therefore:
-	# to_currency --> 1£ = y * (1/x) * 1$ <-- from_currency, 
-	# exchange rate from $ to £ is y/x
+	# Return the exchange rate between from_currency and to_currency on date as a float.
+	# Raises an exception if unable to calculate requested rate.
+	# Raises an exception if there is no rate for the date provided.
+	def self.rate(date, from_currency, to_currency)
+		error_header_string = "DATA RETRIEVAL ERROR: "
 
-
-	# TODO: calculate and return rate
-end
+		# Get the date in string format. Get the input capitalized as needed by the dataset
+		date = date.to_s
+		from_currency = from_currency.upcase
+		to_currency = to_currency.upcase
+	
+		exchange_rates = get_data()
+		rate = -1.0  #default return in case some error occurs. 
+	
+		if !exchange_rates.nil? #check whether the get_data method managed to retrieve some data. If it didn't, 
+			begin
+				if from_currency == CURRENCY_OF_REFERENCE
+					rate = exchange_rates[date][to_currency]
+					raise InputError, "date not found" if exchange_rates[date].nil?
+					raise InputError, "to_currency not found" if rate.nil?
+				elsif to_currency == CURRENCY_OF_REFERENCE
+					rate = 1/exchange_rates[date][from_currency]
+					raise InputError, "date not found" if exchange_rates[date].nil?
+					raise InputError, "from_currency not found" if rate.nil?
+				else
+					# 1$ = x*1€
+					# 1£ = y*1€, therefore:
+					# to_currency --> 1£ = y * (1/x) * 1$ <-- from_currency, 
+					# exchange rate from $ to £ is y/x
+					from_er = exchange_rates[date][from_currency]
+					raise InputError, "date not found" if exchange_rates[date].nil?
+					raise InputError, "from_currency not found" if from_er.nil?
+					to_er = exchange_rates[date][to_currency]
+					raise InputError, "to_currency not found" if to_er.nil?
+					rate = to_er/from_er 
+				end
+				
+			rescue NoMethodError, TypeError, InputError => e
+				puts error_header_string + e.to_s
+			rescue => e
+				puts error_header_string + e.to_s
+				puts e.backtrace.inspect
+			end
+		else
+			puts "No data was retrieved"
+		end
+	
+		return rate
+	end
 
 end
